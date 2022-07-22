e58131311d62a09a98c2d32f409de7b33a75b8ec
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-07-22 13:04:44 -0700

    commit tree 4792ca9edfc1500736f513b945953b982605704d

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,14 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie(dev, item_name, value)
+        self.freebies << Freebie.create(item_name: item_name, value: value, dev_id: dev.id)
+    end
+
+    def self.oldest_company
+        year = self.minimum(:founding_year)
+        self.where("founding_year = #{year}")
+    end
 
 end
